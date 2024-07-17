bfaac175bfaa08943911cf6c3fb1bb20c9f1fca4
SheilAPK <karaulov.z@mail.ru>
2024-07-17 15:03:02 +0300

    Кастомный select

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -3,5 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
   burger.addEventListener('click', () => {
     burger.classList.toggle('burger_close');
     document.querySelector('.header__menu').classList.toggle('phone-open');
-  })
+  });
+
+  const customSelect = document.querySelector(".js-choice");
+  const choices = new Choices(customSelect, {
+    searchEnabled: false,
+    itemSelectText: "",
+  });
 });
