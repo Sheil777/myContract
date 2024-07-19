6fdb8dd1bc3a2186245871e70a1efc7676a0cbe8
SheilAPK <karaulov.z@mail.ru>
2024-07-19 09:00:12 +0300

    Алгоритм для скрытия блоков

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -10,4 +10,32 @@ document.addEventListener('DOMContentLoaded', () => {
     searchEnabled: false,
     itemSelectText: "",
   });
+
+  // 1. Группа кнопок, по которым кликаем
+  // 2. Нужная кнопка
+  // 3. Блок для скрытия
+  var blocksToHide = [
+    ['input[name="mileage-radio"]', '#mileage-no', '.tohide7'],
+    ['input[name="breakdowns"]', '#breakdowns-off', '.tohide8'],
+    ['input[name="additionally"]', '#additionally-no', '.tohide9'],
+  ];
+
+  blocksToHide.forEach(block => {
+    const mileageRadioButtons = document.querySelectorAll(block[0]);
+    mileageRadioButtons.forEach(button => {
+      const mileageNo = document.querySelector(block[1]);
+
+      button.addEventListener('change', () => {
+        if(mileageNo.checked){
+          document.querySelector(block[2]).classList.add('hidden');
+        }else{
+          document.querySelector(block[2]).classList.remove('hidden');
+        }
+      })
+    });
+  });
+
+
+  
+  
 });
